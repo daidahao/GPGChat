24d43a6ec0e874b6f6506567d76102bc445d4c1c
Da Hao <v@daidahao.me>
2017-12-17 14:27:40 +0800

    add starter for GUI

--- ui/starter.py
+++ ui/starter.py
@@ -87,33 +87,69 @@ class LockDialogMod(LockDialog):
             self.Destroy()
 
 data1 = {
-    1: ("Dai", "daidahao@icloud.com"),
-    2: ("Zou", "999@test.com"),
-    3: ("Test", "test@test.com"),
+    1: ("Dai", "daidahao@icloud.com", "9999999"),
+    2: ("Zou", "999@test.com", "9999999"),
+    3: ("Sun", "test@test.com", "9999999"),
 }
 
 
 class MainFrameMod(MainFrame):
+
     def __init__(self, parent):
         MainFrame.__init__(self, parent)
         self.PopulateList()
+        self.count = 0
+        self.allText = []
 
     def PopulateList(self):
 
         # for normal, simple columns, you can add them like this:
         self.list.InsertColumn(0, "Email")
         self.list.InsertColumn(1, "Name")
+        self.list.InsertColumn(2, "KeyID")
 
         items = data1.items()
         for key, data in items:
             index = self.list.InsertItem(self.list.GetItemCount(), data[0])
             self.list.SetItem(index, 1, data[1])
+            self.list.SetItem(index, 2 ,data[2])
             self.list.SetItemData(index, key)
 
-        self.list.SetColumnWidth(0, wx.LIST_AUTOSIZE)
-        self.list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+        self.list.SetColumnWidth(0, 50)
+        self.list.SetColumnWidth(1, 150)
+        self.list.SetColumnWidth(2, 100)
 
         self.currentItem = 0
+
+    def OnSend( self, event ):
+        self.AddSendMessage(self.inputText.GetValue())
+
+    def AddSendMessage(self, message):
+        self.AddMessage(send=True, message=message)
+
+    def AddRecvMessage(self, message):
+        self.AddMessage(send=False, message=message)
+
+    def AddMessage(self, send=True, message=""):
+        wrapSizer = self.m_scrolledWindow1.GetSizer()
+        self.count = self.count + 1
+
+        textCtrl = wx.StaticText(self.m_scrolledWindow1, wx.ID_HIGHEST + 1, u"", wx.DefaultPosition,
+                                 wx.DefaultSize, 0)
+        textCtrl.SetLabelText(message)
+        textCtrl.Wrap(250)
+        textCtrl.SetForegroundColour(wx.Colour(255, 255, 255))
+        textCtrl.SetBackgroundColour(wx.Colour(0, 128, 255))
+
+        if (send):
+            wrapSizer.Add(textCtrl, 0, wx.ALL | wx.ALIGN_RIGHT, 5)
+        else:
+            wrapSizer.Add(textCtrl, 0, wx.ALL | wx.ALIGN_LEFT, 5)
+
+
+        self.m_scrolledWindow1.SetSizerAndFit(wrapSizer)
+        self.m_scrolledWindow1.Layout()
+        self.m_panel15.Layout()
 
 
 if __name__ == '__main__':
