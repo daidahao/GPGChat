b957a7a04fb63a1f1558edf4a51b91345e33bc50
Da Hao <v@daidahao.me>
2017-12-24 13:15:17 +0800

    can set up a lock now

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GPGChat
+
+## Set up
+
+`pip install -U wxPython`
+`pip install cryptography`
+
+## Run the demo
+
+`pythonw gpgchat.py`
--- encrypt/info.py
+++ encrypt/info.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def readinfo(data):
+import base64
+
+
+def readinfo(filepath):
     mail = ''
     salt = ''
     try:
-        reader = open(data, 'r')
+        reader = open(filepath, 'r')
         while True:
             line = reader.readline()
             if len(line) == 0:
                 break
             if line.startswith('mail='):
-                mail=line[5:]
+                mail = line[5:].strip()
             if line.startswith('salt='):
-                salt=line[5:]
+                salt = line[5:].strip()
+                salt = base64.urlsafe_b64decode(salt)
         reader.close()
     except FileNotFoundError as e:
         return False
-    return mail.strip(), salt.strip()
+    return mail, salt
+
 
 def writeinfo(mail, salt, filepath):
+    salt = base64.urlsafe_b64encode(salt).decode()
     f = open(filepath, 'w')
     f.write('mail=' + mail + '\n' + 'salt=' + salt)
     f.close()
